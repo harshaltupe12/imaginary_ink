b0fd19e88eff27f7a9ff722205aae61640658a67
Harshal Tupe <harshaltupe12@gmail.com>
2024-02-28 23:32:02 +0530

    Changes done....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter,
-  Link, 
-  Route,
-  Routes
-} from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import {logo} from './assets'
 import { Home, CreatePost } from "./pages";
 const App = () => {
