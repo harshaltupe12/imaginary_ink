dabd8515f2e7c3c49939020d0568ef3527d33919
Harshal Tupe <harshaltupe12@gmail.com>
2024-03-06 23:09:58 +0530

    Fixing bugs in dalle route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,14 +13,15 @@ router.route('/').get((req, res)=>{
 router.route('/').post(async (req, res)=>{
     try {
         const {prompt} = req.body;
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
-          const image = aiResponse.data.data[0].b64_json;
-          res.status(200).json({ photo: image });
+        const image = aiResponse.data[0].b64_json;
+        res.status(200).json({ photo: image });
+        
     } catch (error) {
         console.error(error);
         res.status(500).send(error?.response.data.error.message || 'Something went wrong');
